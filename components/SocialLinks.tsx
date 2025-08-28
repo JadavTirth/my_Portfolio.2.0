6fcb05629c67a534d3e07e133b582fa97e1bdc50
JadavTirth95 <tirth.jadav04@gmail.com>
2025-08-28 16:46:11 +0530

    add

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -10,7 +10,7 @@ export default function SocialLinks() {
     },
     {
       icon: Linkedin,
-      href: "https://linkedin.com/in/tirth-jadav",
+      href: "https://www.linkedin.com/in/jadav-tirth-138353320?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
       label: "LinkedIn",
     },
     {
